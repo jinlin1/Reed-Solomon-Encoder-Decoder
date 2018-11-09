e4dedfa0d90ea4f5e0697b10c8ba2da2bf93c98f
kindless <hx41455@umbc.edu>
2018-11-09 11:25:05 -0500

    files

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "GaloisFieldArithmetic/GaloisField.h"
+#include "GaloisFieldArithmetic/GaloisFieldElement.h"
+#include "GaloisFieldArithmetic/GaloisFieldPolynomial.h"
+
+using namespace std;
+
+int main(){
+  cout << "hello galois field world" << endl;
+  return 0;}
