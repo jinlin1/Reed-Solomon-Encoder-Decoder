b9861ddc35118e5b80fc81c89b0813efbf6b94be
Jin Lin <jin.lin@sofiac.us>
2018-11-21 10:56:07 -0500

    update encoder to use galois 2^3

--- encoder.cpp
+++ encoder.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <math.h> 
 #include "GaloisFieldArithmetic/GaloisField.h"
 #include "GaloisFieldArithmetic/GaloisFieldElement.h"
 #include "GaloisFieldArithmetic/GaloisFieldPolynomial.h"
 
 int main() {
 
-  const unsigned int symbol_length = 8;
-  const unsigned int codeword_length = 4;
-  const unsigned int data_length = 2;
+  const unsigned int galois_field_exp = 3;
+  const unsigned int codeword_length = pow(2,3) - 1;
+  const unsigned int data_length = 4;
   unsigned int parity_length = codeword_length - data_length;
 
   // Primitive polynomial that acts on the individual symbols 
-  unsigned int prim_poly[symbol_length+1] = {1,1,1,0,0,0,0,1,1};
+  unsigned int prim_poly[galois_field_exp+1] = {1,0,1,1};
 
   // Define the galois field for the individual symbols
-  galois::GaloisField gf(symbol_length, prim_poly);
+  galois::GaloisField gf(galois_field_exp, prim_poly);
 
   // Create individual symbols as an array of symbols
   galois::GaloisFieldElement gfe[data_length] = {
-    galois::GaloisFieldElement(&gf, 255),
-    galois::GaloisFieldElement(&gf, 35)
+    galois::GaloisFieldElement(&gf, 5),
+    galois::GaloisFieldElement(&gf, 3)
   };
 
   // Transform the array of symbols into a polynomial
