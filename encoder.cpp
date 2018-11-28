86396bd2952ef8db779e5dc6168cda5a2cdd2d49
kindless <hx41455@umbc.edu>
2018-11-28 18:37:37 -0500

    encoder now takes in a text file, and should be theoretically completely dynamic

--- encoder.cpp
+++ encoder.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
-#include <math.h> 
+#include <math.h>
 #include "GaloisFieldArithmetic/GaloisField.h"
 #include "GaloisFieldArithmetic/GaloisFieldElement.h"
 #include "GaloisFieldArithmetic/GaloisFieldPolynomial.h"
+using namespace std;
 
-int main() {
+int init(vector<int> prim, vector<int> gen, vector<int> msg) {
 
   const unsigned int galois_field_exp = 3;
-  const unsigned int codeword_length = pow(2,3) - 1;
-  const unsigned int data_length = 3;
+  const unsigned int codeword_length = pow(2,galois_field_exp) - 1;
+  const unsigned int data_length = msg.size();
   unsigned int parity_length = codeword_length - data_length;
 
   // Primitive polynomial that acts on the individual symbols 
@@ -19,67 +21,84 @@ int main() {
   // The order is swapped from which it is written.
   // p(x) = 1x^3+0x^2+1x^1+1x^0
   //        1    0    1    1
-  unsigned int prim_poly[galois_field_exp+1] = {1,1,0,1};
+  unsigned int prim_poly[prim.size()];
+  for (int i=0; i < prim.size(); i++) {
+    prim_poly[i] = prim.at(i);
+  }
 
+    
   // Define the galois field for the individual symbols
   galois::GaloisField gf(galois_field_exp, prim_poly);
 
   // Create individual symbols as an array of symbols
-  galois::GaloisFieldElement gfe[data_length] = {
-    galois::GaloisFieldElement(&gf, 5),
-    galois::GaloisFieldElement(&gf, 3),
-    galois::GaloisFieldElement(&gf, 2)
-  };
+  galois::GaloisFieldElement msg_gfe[msg.size()];
+  for (int i=0; i<msg.size();i++) {
+    msg_gfe[i] = galois::GaloisFieldElement(&gf, msg.at(i));
+  }
 
   // Transform the array of symbols into a polynomial
-  galois::GaloisFieldPolynomial polynomial(&gf,data_length-1,gfe);
+  galois::GaloisFieldPolynomial msg_polynomial(&gf,data_length-1,msg_gfe);
 
   // Shift the polynomial by the parity length 
-  polynomial = polynomial << parity_length;
+  msg_polynomial = msg_polynomial << parity_length;
 
-  std::cout << "Shifted message polynomial: " << polynomial << "\n";
+  cout << "Shifted message polynomial: " << msg_polynomial << "\n";
 
-  galois::GaloisFieldElement gfe1[2] = {
-    galois::GaloisFieldElement(&gf, 2),
-    galois::GaloisFieldElement(&gf, 1),
-  };
+ 
+  vector<galois::GaloisFieldElement(*)> gen_gfe;
+  vector<galois::GaloisFieldPolynomial> gen_polynomial;
 
-  galois::GaloisFieldElement gfe2[2] = {
-    galois::GaloisFieldElement(&gf, 4),
-    galois::GaloisFieldElement(&gf, 1),
-  };
+  for (int i=0; i < gen.size(); i++) {
+    galois::GaloisFieldElement gfei[2] = {galois::GaloisFieldElement(&gf, gen.at(i)),
+					  galois::GaloisFieldElement(&gf, 1)};
+    gen_gfe.push_back(gfei);
+    gen_polynomial.push_back(galois::GaloisFieldPolynomial(&gf, 1, gen_gfe.at(i)));
+    cout << "Generator polynomial term" << i+1 << ": " << gen_polynomial.at(i) << "\n";
+  }
 
-  galois::GaloisFieldElement gfe3[2] = {
-    galois::GaloisFieldElement(&gf, 3),
-    galois::GaloisFieldElement(&gf, 1),
-  };
+  galois::GaloisFieldPolynomial generator_polynomial = gen_polynomial.at(0);
 
-  galois::GaloisFieldElement gfe4[2] = {
-    galois::GaloisFieldElement(&gf, 6),
-    galois::GaloisFieldElement(&gf, 1),
-  };
+  for (int i=1; i < gen_polynomial.size(); i++) {
+    generator_polynomial *= gen_polynomial.at(i);
+  }
 
-  galois::GaloisFieldPolynomial polynomial1(&gf,1,gfe1);
-  galois::GaloisFieldPolynomial polynomial2(&gf,1,gfe2);
-  galois::GaloisFieldPolynomial polynomial3(&gf,1,gfe3);
-  galois::GaloisFieldPolynomial polynomial4(&gf,1,gfe4);
+  cout << "Generator polynomial: " << generator_polynomial << "\n";
 
-  std::cout << "Generator polynomial term 1: " << polynomial1 << "\n";
-  std::cout << "Generator polynomial term 2: " << polynomial2 << "\n";
-  std::cout << "Generator polynomial term 3: " << polynomial3 << "\n";
-  std::cout << "Generator polynomial term 4: " << polynomial4 << "\n";
+  galois::GaloisFieldPolynomial parity_polynomial = msg_polynomial % generator_polynomial; 
 
-  galois::GaloisFieldPolynomial generator_polynomial = (polynomial1 * polynomial2 * polynomial3 * polynomial4);
+  cout << "Parity polynomial: " << parity_polynomial << "\n";
 
-  std::cout << "Generator polynomial: " << generator_polynomial << "\n";
+  galois::GaloisFieldPolynomial message = msg_polynomial + parity_polynomial;
 
-  galois::GaloisFieldPolynomial parity_polynomial = polynomial % generator_polynomial; 
-
-  std::cout << "Parity polynomial: " << parity_polynomial << "\n";
-
-  galois::GaloisFieldPolynomial message = polynomial + parity_polynomial;
-
-  std::cout << "Encoded messgae polynomial: " << message << "\n";
-
+  cout << "Encoded messgae polynomial: " << message << "\n";
+  
   return 0;
 }
+
+int main() {
+
+  ifstream infile("input.txt");
+  string temp;
+  vector<int> primpoly,genpoly,msg;
+
+  if (getline(infile, temp)) {
+    for (int i=0; i<temp.length(); i++) {
+      primpoly.push_back(temp.at(i) - '0');
+    }    
+  } else { return -1; }
+  if (getline(infile, temp)) {
+    for (int i=0; i<temp.length(); i++) {
+      genpoly.push_back(temp.at(i) - '0');
+    }
+  } else { return -1; }
+  if (getline(infile, temp)) {
+    for (int i=0; i<temp.length(); i++) {
+      msg.push_back(temp.at(i) - '0');
+    }
+  } else { return -1; }
+
+  init(primpoly, genpoly, msg);  
+  
+  return 0;
+}
+
